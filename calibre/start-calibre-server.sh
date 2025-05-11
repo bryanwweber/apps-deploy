64454002f849551c60b71c1fdbf7357a65c8c19a
Bryan Weber <bryan.w.weber@gmail.com>
2025-05-11 14:03:00 -0400

    Listen on all addresses

--- calibre/start-calibre-server.sh
+++ calibre/start-calibre-server.sh
@@ -3,7 +3,7 @@
 XDG_RUNTIME_DIR=/tmp/runtime-root exec /usr/bin/calibre-server \
     --disable-use-bonjour \
     --enable-local-write \
-    --listen-on 127.0.0.1 \
+    --listen-on 0.0.0.0 \
     --auth-mode=basic \
     "$@" \
     "/library"
