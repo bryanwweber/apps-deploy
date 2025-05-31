2395ed0b9ae6ebcb0be7044b2971cd885599b4b6
Bryan Weber <bryan.w.weber@gmail.com>
2025-05-31 17:23:02 -0400

    Fix user directory and simplify startup

--- calibre/start-calibre-server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-XDG_RUNTIME_DIR=/tmp/runtime-root exec /usr/bin/calibre-server \
-    --disable-use-bonjour \
-    --enable-local-write \
-    --listen-on 0.0.0.0 \
-    --auth-mode=basic \
-    "$@" \
-    "/library"
